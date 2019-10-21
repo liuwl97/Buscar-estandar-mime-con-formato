a698faadda588a872a409e6b921b9b94f5cec61e
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2019-10-21 16:18:07 -0300

    Update buscarEstandarMIME.php

--- buscarEstandarMIME.php
+++ buscarEstandarMIME.php
@@ -1,45 +1,21 @@
-function buscarEstandarMIME($formato){
+function buscarEstandarMIMENombre($filename){
+    return mime_content_type($filename);
+}
+
+function buscarEstandarMIMEFormato($formato){
     //Encuentra el estandar MIME https://developer.mozilla.org/es/docs/Web/HTTP/Basics_of_HTTP/MIME_types
-    $standarMIME  = array(
-        'jpg'  => 'image/jpg',
-        'gif'  => 'image/gif',
-        'png'  => 'image/png',
-        'zip'  => 'application/x-compressed',
-        'txt'  => 'text/plain',
-        'pdf'  => 'application/pdf',
-        'doc'  => 'application/msword',
-        'dot'  => 'application/msword',
-        'docx' => 'pplication/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        'dotx' => 'pplication/vnd.openxmlformats-officedocument.wordprocessingml.template',
-        'docm' => 'pplication/vnd.ms-word.document.macroEnabled.12',
-        'dotm' => 'pplication/vnd.ms-word.template.macroEnabled.12',
-        'xls'  => 'application/vnd.ms-excel',
-        'xlt'  => 'application/vnd.ms-excel',
-        'xla'  => 'application/vnd.ms-excel',
-        'xlsx' => 'pplication/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        'xltx' => 'pplication/vnd.openxmlformats-officedocument.spreadsheetml.template',
-        'xlsm' => 'pplication/vnd.ms-excel.sheet.macroEnabled.12',
-        'xltm' => 'pplication/vnd.ms-excel.template.macroEnabled.12',
-        'xlam' => 'pplication/vnd.ms-excel.addin.macroEnabled.12',
-        'xlsb' => 'pplication/vnd.ms-excel.sheet.binary.macroEnabled.12',
-        'ppt'  => 'application/vnd.ms-powerpoint',
-        'pot'  => 'application/vnd.ms-powerpoint',
-        'pps'  => 'application/vnd.ms-powerpoint',
-        'ppa'  => 'application/vnd.ms-powerpoint',
-        'pptx' => 'pplication/vnd.openxmlformats-officedocument.presentationml.presentation',
-        'potx' => 'pplication/vnd.openxmlformats-officedocument.presentationml.template',
-        'ppsx' => 'pplication/vnd.openxmlformats-officedocument.presentationml.slideshow',
-        'ppam' => 'pplication/vnd.ms-powerpoint.addin.macroEnabled.12',
-        'pptm' => 'pplication/vnd.ms-powerpoint.presentation.macroEnabled.12',
-        'potm' => 'pplication/vnd.ms-powerpoint.template.macroEnabled.12',
-        'ppsm' => 'pplication/vnd.ms-powerpoint.slideshow.macroEnabled.12',
-        'mdb'  => 'application/vnd.ms-access'
-    );
-    $mime = $standarMIME[$formato];
-    return $mime;
+    return mime_content_type("example.$filename");
 }
 
 echo '<h1>Aquí se mostrarán un par de ejemplos</h1>';
-echo '<p>buscarEstandarMIME('pdf').'</p>';
-echo '<p>buscarEstandarMIME('png').'</p>';
-echo '<p>buscarEstandarMIME('doc').'</p>';
+echo '<p>buscarEstandarMIMEFormato('pdf').'</p>';
+echo '<p>buscarEstandarMIMEFormato('png').'</p>';
+echo '<p>buscarEstandarMIMEFormato('doc').'</p>';
+
+
+
+echo '<h1>Aquí se mostrarán un par de ejemplos</h1>';
+
+echo '<p>buscarEstandarMIMENombre('tarea6.pdf').'</p>';
+echo '<p>buscarEstandarMIMENombre('imagen.png').'</p>';
+echo '<p>buscarEstandarMIMENombre('documento.doc').'</p>';
